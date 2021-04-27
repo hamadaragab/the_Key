8eee457dc94ae5c4ad01145c40475f8dc52f79c4
Hamada Ragab <hamadaragab550@gmail.com>
2021-04-27 14:15:42 +0200

    my comment

--- The Key Discount/Sences/HomeModule/HomeTabBar/View/Controller/HomeTabBarViewController.swift
+++ The Key Discount/Sences/HomeModule/HomeTabBar/View/Controller/HomeTabBarViewController.swift
@@ -11,6 +11,7 @@ class HomeTabBarViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("relaesed")
 
         // Do any additional setup after loading the view.
     }
